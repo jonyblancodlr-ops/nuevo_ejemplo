d0a9cc206e00668ab331fdedb63a4750959f807d
jonyblancodlr-ops <jonyblanco.dlr@gmail.com>
2026-05-23 12:45:14 -0600

    Funciones primeras

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+import tkinter as tk #Acceso al modulo de la interfaz
+from tkinter import * # Importa todas las clases y funciones que tiene la libreria
+
+#Venatana principal
+ventana = tk.Tk()
+ventana.title("Calculadora basica") #Nombre a la ventana
+ventana.geometry("450x250") # tamaño ancho x alto
+
+#Funciones
+
+def sumar():
+    # .get() obtiene el valor e int () lo convierte a entero para la suma
+    resultado_numerico = int(numero1.get()) + int(numero2.get())
+    lbl_res.config(text = resultado_numerico)
+    
+def restar():
+    # .get() obtiene el valor e int () lo convierte a entero para la suma
+    resultado_numerico = int(numero1.get()) - int(numero2.get())
+    lbl_res.config(text = resultado_numerico)  
+
+def multiplicar():
+        # .get() obtiene el valor e int () lo convierte a entero para la suma
+    resultado_numerico = int(numero1.get()) * int(numero2.get())
+    lbl_res.config(text = resultado_numerico)
+
+def dividir():
+        # .get() obtiene el valor e int () lo convierte a entero para la suma
+    resultado_numerico = int(numero1.get()) / int(numero2.get())
+    lbl_res.config(text = resultado_numerico)
